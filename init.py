814ad86a2c8cd62a3a5e0a2959ab23f91b57fb9e
ankraft <an.kraft@googlemail.com>
2019-03-10 15:51:31 +0100

    Simplified request calls. Added markdown for request outputs.

--- init.py
+++ init.py
@@ -1,22 +1,51 @@
-# Configuration
+############################# Configuration ##############################
 
 url              = 'http://localhost:8080/in-name'     # The URL of the CSE we want to connect to
 originator       = 'admin:admin'                       # Originator ID to access the CSE
 notificationPort = 8000                                # Port that the Notification Server binds to
 
 
-########################## End of  Configuration ##########################
+########################## End of Configuration ##########################
 
 
 import requests, json
+import IPython.display
+
+# Print and format as Markdown
+def printmd(s):
+    IPython.display.display(IPython.display.Markdown(s))
+
+# Print the request
+def printRequest(url):
+    printmd('Sending request to **' + url + '**')
 
 # Tidy-print a response header.
 def printResponse(r):
+    printmd('---\n### Response')
     print(str(r.status_code) + ' (' + r.reason + ')')
-    print('========== Headers ==========')
+    printmd('\n**Headers**')
     for h in r.headers:
         print(h + ':' + r.headers[h])
     if r.text:
-        print('\n========== Body ==========')
+        printmd('\n**Body**\n')
         print(r.text)
+    printmd('---')
+
+# The following request methods hide a bit of the complexity of constructing the
+# requests and make them easier to read.
+
+def GET(url, headers):
+    printRequest(url)
+    printResponse(requests.get(url, headers=headers))
+
+def POST(url, headers, body):
+    printRequest(url)
+    printResponse(requests.post(url, headers=headers, data=body))
+
+def DELETE(url, headers):
+    printRequest(url)
+    printResponse(requests.delete(url, headers=headers))
     
+def PUT(url, headers, body):
+    printRequest(url)
+    printResponse(requests.put(url, headers=headers, data=body))

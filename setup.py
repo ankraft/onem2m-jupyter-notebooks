40b0fbec476c56514e8f35e8b7d113392424ea32
ankraft <an.kraft@gmail.com>
2020-06-25 13:08:13 +0200

    Added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='oneM2M-jupyter-notebooks',
+    version='1.0.0',
+    url='https://github.com/ankraft/onem2m-jupyter-notebooks',
+    author='Andreas Kraft',
+    author_email='an.kraft@gmail.com',
+    description='A couple of Jupyter notebooks to explain how to create, access and manage oneM2M resources',
+    packages=find_packages(),
+	install_requires=['flask', 'psutil',  'requests', 'tinydb' ]
+)

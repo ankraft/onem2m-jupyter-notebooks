12d6b42bb1372c78cb11f616570c0e3c475759ad
ankraft <an.kraft@gmail.com>
2022-04-01 15:42:27 +0200

    Corrected wrong dependencies

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='oneM2M-jupyter-notebooks',
-    version='1.3.0',
-    url='https://github.com/ankraft/onem2m-jupyter-notebooks',
-    author='Andreas Kraft',
-    author_email='an.kraft@gmail.com',
-    description='A couple of Jupyter notebooks to explain how to create, access and manage oneM2M resources',
-    packages=find_packages(),
-    install_requires=[
-        'jupyter',
-        'jupyterlab',
-        'cbor2',
-        'flask',
+	name='oneM2M-jupyter-notebooks',
+	version='1.3.0',
+	url='https://github.com/ankraft/onem2m-jupyter-notebooks',
+	author='Andreas Kraft',
+	author_email='an.kraft@gmail.com',
+	description='A couple of Jupyter notebooks to explain how to create, access and manage oneM2M resources',
+	packages=find_packages(),
+	install_requires=[
+		'jupyter',
+		'jupyterlab',
+		'cbor2',
+		'flask',
 		'InquirerPy',
-        'isodate', 
+		'isodate', 
 		'paho-mqtt',
-        'requests', 
-        'rich', 
-        'tinydb'
-     ]
+		'requests', 
+		'rich', 
+		'tinydb'
+	 ]
 )
